4810912a9d4ca66c22b651307e72cb59b1f65f0f
JJGarcia24 <40121872+jasonjgarcia24@users.noreply.github.com>
2021-12-16 11:05:43 -0800

    Created brownie-simple-storage project.

--- simple-storage/scripts/deploy.py
+++ simple-storage/scripts/deploy.py
@@ -14,7 +14,7 @@ ABI_DIR = lambda fn: os.path.join(BASE_DIR, f"artifacts\\{fn}")
 CONTRACTS_DIR = lambda fn: os.path.join(BASE_DIR, f"contracts\\{fn}")
 
 # Load environment variables.
-config = dotenv_values(".env")  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
+config = dotenv_values(".env")
 
 # Read in contract
 with open(CONTRACTS_DIR("SimpleStorage.sol"), "r") as file:
@@ -45,10 +45,10 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # Connect to Ganache
-w3 = Web3(Web3.HTTPProvider(config["GANACHE_RPC_URL"]))
-chain_id = int(config["GANACHE_CHAIN_ID"])
-my_public_address = config["GANACHE_1_PUBLIC_KEY"]
-my_private_address = config["GANACHE_1_PRIVATE_KEY"]
+w3 = Web3(Web3.HTTPProvider(config["INFURA_KOVAN_URL_KEY"]))
+chain_id = int(config["KOVAN_CHAIN_ID"])
+my_public_address = config["PRIMARY_PUBLIC_KEY"]
+my_private_address = config["PRIMARY_PRIVATE_KEY"]
 
 # Create the contract in python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
@@ -68,6 +68,36 @@ transaction = SimpleStorage.constructor().buildTransaction(
 )
 
 signed_transaction = w3.eth.account.sign_transaction(
-    transaction,
-    private_key=my_private_address
+    transaction, private_key=my_private_address
 )
+
+transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
+
+# Working with the contract, you always need
+# Cotract Address
+# contract ABI
+simple_storage = w3.eth.contract(address=transaction_receipt.contractAddress, abi=abi)
+# Call -> Simulate making the call and getting a return value
+# Transact -> Actually make a state change
+print(simple_storage.functions.retrieve().call())
+
+# Initial value of favoriteNumber
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "chainId": chain_id,
+        "from": my_public_address,
+        "nonce": nonce + 1,
+    }
+)
+
+signed_store_transaction = w3.eth.account.sign_transaction(
+    store_transaction, private_key=my_private_address
+)
+
+send_transaction_hash = w3.eth.send_raw_transaction(
+    signed_store_transaction.rawTransaction
+)
+
+send_transaction_receipt = w3.eth.wait_for_transaction_receipt(send_transaction_hash)
+print(simple_storage.functions.retrieve().call())
